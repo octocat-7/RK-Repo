d424ec1531bdd454a948e762101229890bd6bccb
Rupesh Kumar <86493111+octocat-7@users.noreply.github.com>
2023-06-18 14:26:14 +0530

    Update HealthBondScheme.php

--- HealthBondScheme.php
+++ HealthBondScheme.php
@@ -8,13 +8,9 @@ added new policies into this bond
 customized requirements to be included...by client
 
 Modifications-1
-
 UserCategory
-<<<<<<< Updated upstream
-Registration_Category
-=======
 Registration_Category
 
 Modifications-2
 Working on Backend Designing for this task
->>>>>>> Stashed changes
+
