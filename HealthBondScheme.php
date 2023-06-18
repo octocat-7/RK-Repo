ed4125cabf9f99834799721dbd10e0c126cdd297
octocat-7 <rupesh.kalashika@gmail.com>
2023-06-18 14:10:00 +0530

    new changes

--- HealthBondScheme.php
+++ HealthBondScheme.php
@@ -1,8 +1,8 @@
 working with new feature branch by rupesh
 
 ID
-EligD[B[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D
-bond_purchase_Data[D[D[D[[C[C[C[C[C
+Eligibility
+bond_purchase_Data
 new modifications by developer4
-added new poliies [D[D[D[Dc[C[C[D[Dies into this bond
+added new policies into this bond
 customized requirements to be included...by client
