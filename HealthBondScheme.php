529b91d598b9b04d47c093266dbf32f6f7ef504f
octocat-7 <rupesh.kalashika@gmail.com>
2023-06-18 14:19:55 +0530

    New Modifications on HBS.php file

--- HealthBondScheme.php
+++ HealthBondScheme.php
@@ -6,3 +6,8 @@ bond_purchase_Data
 new modifications by developer4
 added new policies into this bond
 customized requirements to be included...by client
+
+Modifications-1
+
+UserCategory
+Registration_Category
