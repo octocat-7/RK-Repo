44bc17fb005774c4e0a6ade8ad37ad8760bfbd3a
octocat-7 <rupesh.kalashika@gmail.com>
2023-06-21 10:44:20 +0530

    New changes in HBS.php file

--- HealthBondScheme.php
+++ HealthBondScheme.php
@@ -10,11 +10,7 @@ customized requirements to be included...by client
 Modifications-1
 
 UserCategory
-<<<<<<< Updated upstream
-Registration_Category
-=======
 Registration_Category
 
 Modifications-2
 Working on Backend Designing for this task
->>>>>>> Stashed changes
