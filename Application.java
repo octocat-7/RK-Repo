6a86906fe65c1c75474f1c0d0503a1d571e3fdc6
octocat-7 <rupesh.kalashika@gmail.com>
2023-06-23 16:44:20 +0530

    New changes in this file

--- Application.java
+++ Application.java
@@ -43,4 +43,19 @@ class Main {
     System.out.println("Sum is: " + result);
   }
 }
+public class Main {
+
+  public static void main(String[] args) {
+
+    int count = 0, num = 0003452;
+
+    while (num != 0) {
+      // num = num/10
+      num /= 10;
+      ++count;
+    }
+
+    System.out.println("Number of digits: " + count);
+  }
+}
 }
